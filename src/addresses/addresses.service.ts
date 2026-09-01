82bd9ec79f884dad096b8766425d854645d38f8d
Harits Ghiffari Hanif <harits.gh1@gmail.com>
2026-09-01 12:06:02 +0700

    fix: changed operand to match the logic of different user info

--- src/addresses/addresses.service.ts
+++ src/addresses/addresses.service.ts
@@ -77,7 +77,7 @@ export class AddressesService {
 
   //Utils
   isMyAddress(userId: string, Address: Address) {
-    if (Address.userId === userId) return false;
+    if (Address.userId !== userId) return false;
     return true;
   }
 }
